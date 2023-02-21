c93bbf8fe972fe7b1f878c336a15ce132d29a1ad
haha23thereal <46943103+haha23thereal@users.noreply.github.com>
2023-02-21 09:24:18 -0800

    Enumaration

--- 5 - Enumerations/Lab - Enumerations.playground/Pages/2. App Exercise - Swimming Workouts.xcplaygroundpage/Contents.swift
+++ 5 - Enumerations/Lab - Enumerations.playground/Pages/2. App Exercise - Swimming Workouts.xcplaygroundpage/Contents.swift
@@ -46,8 +46,12 @@ print("David swam \(david.distance) meters in \(david.time) seconds, while doing
  
 
 //:  Now add an instance method to `SwimmingWorkout` called `save` that takes no parameters and has no return value. This method will add its instance to the static array on `SwimmingWorkout` that corresponds to its swimming stroke. Inside `save` write a switch statement that switches on the instance's `stroke` property, and appends `self` to the proper array. Call save on the two instances of `SwimmingWorkout` that you created above, and then print the array(s) to which they should have been added to see if your `save()` method works properly.
-
-
+daniel.save()
+david.save()
+print(SwimmingWorkout.freestyle)
+print(SwimmingWorkout.butterfly)
+print(SwimmingWorkout.backstroke)
+print(SwimmingWorkout.breaststroke)
 /*:
  _Copyright © 2021 Apple Inc._
 
