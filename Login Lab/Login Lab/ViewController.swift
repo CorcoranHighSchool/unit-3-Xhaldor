24f051b794bcff9468b78626de04bfc06e3700e3
haha23thereal <46943103+haha23thereal@users.noreply.github.com>
2023-03-06 09:35:01 -0800

    Lab (Broken)

--- Login Lab/Login Lab/ViewController.swift
+++ Login Lab/Login Lab/ViewController.swift
@@ -20,22 +20,28 @@ class ViewController: UIViewController {
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         segue.destination.navigationItem.title = username.text
+        
+            guard let sender = (sender as? UIButton) else {return}
+        if sender == forgotPassWord {
+            segue.destination.navigationItem.title = "Forgot Password" }
+        else if sender == forgotUserName {
+            segue.destination.navigationItem.title = "Forgot Username" }
+        else {
+            segue.destination.navigationItem.title = username.text
+            
+        }
+        
     }
+         
+    
     @IBAction func forgotUser(_ sender: Any) {
-        performSegue(withIdentifier: "forgotUserNameorPassWord", sender: sender)
+        performSegue(withIdentifier: "forgotUserName", sender: sender)
     }
     @IBAction func forgotPass(_ sender: Any) {
-        performSegue(withIdentifier: "forgotUserNameorPassWord", sender: sender)
+        performSegue(withIdentifier: "forgotPassWord", sender: sender)
     }
     
 }
-/*
- guard let sender = (sender as? UIButton else {return}
- if sender == forgotPasswordButton { segue.destination.navigationItem.title = "Forgot Password" }
- else if sender == forgotUsernameButton { segue.destination.navigationItem.title = "Forgot Username" }
- else { segue.destination.navigationItem.title = usernameTextField.text }
- 
- Fails and no matter what I did it crashed the code. Monday remind me.
- */
+
 
 
